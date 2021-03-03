441b418a72e3734a9fced41a33426cc76bf4ff65
HariKrishna <HariKrishna@HariKrishna-PC>
2021-03-03 15:46:13 +0530

    Fist commit....

---
+++ src/test/java/Company/gitPracticeProject/Test1.java
@@ -0,0 +1,14 @@
+package Company.gitPracticeProject;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void firstTest() {
+		assertTrue(false, "Test is Failed...!");
+	}
+
+}
